340c226e28fe2fad0e03a89c61b7886b1132114a
Raphael Michel <mail@raphaelmichel.de>
2020-12-04 23:39:09 +0100

    First pairings

--- server/venueless/live/modules/roulette.py
+++ server/venueless/live/modules/roulette.py
@@ -4,6 +4,7 @@ import websockets
 from django.utils.crypto import get_random_string
 
 from venueless.core.permissions import Permission
+from venueless.core.utils.redis import aioredis
 from venueless.live.decorators import command, room_action
 from venueless.live.exceptions import ConsumerException
 from venueless.live.modules.base import BaseModule
@@ -23,11 +24,42 @@ class RouletteModule(BaseModule):
     async def start(self, body):
         if not self.consumer.user.profile.get("display_name"):
             raise ConsumerException("roulette.start.missing_profile")
+
+        listkey = f"roulette:waiting:{self.room.id}"
+        seen = set()
+        async with aioredis() as redis:
+            while True:
+                room = await redis.lpop(listkey)
+                if not room or room in seen:
+                    break
+                we_talked_to_this_person_recently = False  # todo: implement
+                if we_talked_to_this_person_recently:
+                    await redis.rpush(listkey, room)  # put back on the queue
+                    seen.add(
+                        room
+                    )  # make sure we don't loop endless and break if we see this again
+                    room = None
+                else:
+                    seen.add(room)
+                    break
+
+        # todo: there's a race condition here, if a second person hits start while the first person's room is still
+        # being created on the janus server, they will be in separate rooms, which is bad if no more people join.
+        # can we fully solve this without some locking that would be bad for scalability?
+
+        if room:
+            room = json.loads(room)
+        else:
+            room = await self._create_room()
+            await redis.rpush(listkey, json.dumps(room))
+
+        await self.consumer.send_success(room)
+
+    async def _create_room(self):
         server = (
             "wss://dev-janus.venueless.events/ws"  # todo: choose from multiple servers
         )
         token = get_random_string(16)
-
         # todo: handle connection errors to janus, encapsulate room creation into service
         async with websockets.connect(
             server, subprotocols=["janus-protocol"]
@@ -72,7 +104,9 @@ class RouletteModule(BaseModule):
             )
             resp = json.loads(await websocket.recv())
             room_id = resp["plugindata"]["data"]["room"]
-
-        await self.consumer.send_success(
-            {"server": server, "roomId": room_id, "token": token, "resp": resp}
-        )
+        return {
+            "server": server,
+            "roomId": room_id,
+            "token": token,
+            "creator": str(self.consumer.user.id),
+        }
