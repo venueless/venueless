4568dbbcd9de3b32fec0dbeb89440deb164a9d8d
Raphael Michel <mail@raphaelmichel.de>
2020-12-14 21:56:15 +0100

    Add JanusServer model

--- server/venueless/live/modules/roulette.py
+++ server/venueless/live/modules/roulette.py
@@ -1,9 +1,9 @@
 import json
 
-import websockets
-from django.utils.crypto import get_random_string
+from channels.db import database_sync_to_async
 
 from venueless.core.permissions import Permission
+from venueless.core.services.janus import choose_server, create_room
 from venueless.core.utils.redis import aioredis
 from venueless.live.decorators import command, room_action
 from venueless.live.exceptions import ConsumerException
@@ -30,7 +30,10 @@ class RouletteModule(BaseModule):
         async with aioredis() as redis:
             while True:
                 room = await redis.lpop(listkey)
-                if not room or room in seen:
+                if not room:
+                    break
+                if room in seen:  # we've looped through all requests
+                    await redis.rpush(listkey, room)  # put back on the queue
                     break
                 we_talked_to_this_person_recently = False  # todo: implement
                 if we_talked_to_this_person_recently:
@@ -50,62 +53,15 @@ class RouletteModule(BaseModule):
         if room:
             room = json.loads(room)
         else:
-            room = await self._create_room()
+            server = await database_sync_to_async(choose_server)(
+                world=self.consumer.world
+            )
+            if not server:
+                await self.consumer.send_error(
+                    "roulette.no_server", "No server available"
+                )
+                return
+            room = await create_room(server)
             await redis.rpush(listkey, json.dumps(room))
 
         await self.consumer.send_success(room)
-
-    async def _create_room(self):
-        server = (
-            "wss://dev-janus.venueless.events/ws"  # todo: choose from multiple servers
-        )
-        token = get_random_string(16)
-        # todo: handle connection errors to janus, encapsulate room creation into service
-        async with websockets.connect(
-            server, subprotocols=["janus-protocol"]
-        ) as websocket:
-            await websocket.send(
-                json.dumps({"janus": "create", "transaction": get_random_string()})
-            )
-            resp = json.loads(await websocket.recv())
-            session_id = resp["data"]["id"]
-
-            await websocket.send(
-                json.dumps(
-                    {
-                        "janus": "attach",
-                        "plugin": "janus.plugin.videoroom",
-                        "transaction": get_random_string(),
-                        "session_id": session_id,
-                    }
-                )
-            )
-            resp = json.loads(await websocket.recv())
-            handle_id = resp["data"]["id"]
-
-            await websocket.send(
-                json.dumps(
-                    {
-                        # Docs: https://janus.conf.meetecho.com/docs/videoroom.html
-                        "janus": "message",
-                        # todo: add admin_key and configure server to require that key
-                        "body": {
-                            "request": "create",
-                            "permanent": False,
-                            # todo: set "secret": "…",
-                            "is_private": True,
-                            "allowed": [token],
-                        },
-                        "transaction": get_random_string(),
-                        "session_id": session_id,
-                        "handle_id": handle_id,
-                    }
-                )
-            )
-            resp = json.loads(await websocket.recv())
-            room_id = resp["plugindata"]["data"]["room"]
-        return {
-            "server": server,
-            "roomId": room_id,
-            "token": token,
-        }
