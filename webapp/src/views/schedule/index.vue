760cb71642c7651bdc418666d4f634b5599c1de9
rash <sebastian.gepperth@ax-semantics.com>
2022-01-03 22:19:26 +0100

    use new @pretalx/schedule package

--- webapp/src/views/schedule/index.vue
+++ webapp/src/views/schedule/index.vue
@@ -6,8 +6,29 @@
 		//- 	timezone-changer
 		bunt-tabs.days(v-if="days && days.length > 1", :active-tab="currentDay.toISOString()", ref="tabs", v-scrollbar.x="")
 			bunt-tab(v-for="day in days", :id="day.toISOString()", :header="moment(day).format('dddd DD. MMMM')", @selected="changeDay(day)")
-		grid-schedule(v-if="$mq.above['m']", :currentDay="currentDay", @changeDay="currentDay = $event")
-		linear-schedule(v-else, :currentDay="currentDay", @changeDay="changeDayByScroll")
+		.scroll-parent(ref="scrollParent", v-scrollbar.x.y="")
+			grid-schedule(v-if="$mq.above['m']",
+				:sessions="sessions",
+				:rooms="schedule.rooms",
+				:currentDay="currentDay",
+				:now="now",
+				:scrollParent="$refs.scrollParent",
+				:favs="favs",
+				@changeDay="currentDay = $event",
+				@fav="fav",
+				@unfav="unfav"
+			)
+			linear-schedule(v-else,
+				:sessions="sessions",
+				:rooms="schedule.rooms",
+				:currentDay="currentDay",
+				:now="now",
+				:scrollParent="$refs.scrollParent",
+				:favs="favs",
+				@changeDay="changeDayByScroll",
+				@fav="fav",
+				@unfav="unfa"
+			)
 	.error(v-else-if="errorLoading")
 		.mdi.mdi-alert-octagon
 		h1 {{ $t('schedule/index:scheduleLoadingError') }}
@@ -15,27 +36,43 @@
 </template>
 <script>
 import { mapState, mapGetters } from 'vuex'
+import router from 'router'
 import moment from 'lib/timetravelMoment'
 import TimezoneChanger from 'components/TimezoneChanger'
-import LinearSchedule from 'components/schedule/LinearSchedule'
-import GridSchedule from 'components/schedule/GridSchedule'
+import { LinearSchedule, GridSchedule} from '@pretalx/schedule'
+import '@pretalx/schedule/dist/schedule.css'
 
 export default {
 	components: { LinearSchedule, GridSchedule, TimezoneChanger },
 	data () {
 		return {
 			moment,
-			currentDay: moment().startOf('day')
+			currentDay: moment().startOf('day'),
+			rawFavs: []
 		}
 	},
 	computed: {
-		...mapState('schedule', ['schedule', 'errorLoading']),
-		...mapGetters('schedule', ['days'])
+		...mapState('schedule', ['now', 'schedule', 'errorLoading']),
+		...mapGetters('schedule', ['days', 'sessions']),
+		favs () {
+			return this.pruneFavs(this.rawFavs, this.sessions)
+		}
 	},
-	created () {},
-	mounted () {
-		this.$nextTick(() => {
-		})
+	provide: {
+		linkTarget: '_blank',
+		generateSessionLinkUrl ({session}) {
+			if (session.url) return this.session.url
+			return router.resolve({name: 'schedule:talk', params: {talkId: session.id}}).href
+		},
+		async onSessionLinkClick (event, session) {
+			if (!session.url) {
+				event.preventDefault()
+				await router.push({name: 'schedule:talk', params: {talkId: session.id}})
+			}
+		}
+	},
+	created () {
+		this.rawFavs = this.loadFavs()
 	},
 	methods: {
 		changeDay (day) {
@@ -47,6 +84,36 @@ export default {
 			const tabEl = this.$refs.tabs.$refs.tabElements.find(el => el.id === day.toISOString())
 			// TODO smooth scroll, seems to not work with chrome {behavior: 'smooth', block: 'center', inline: 'center'}
 			tabEl?.$el.scrollIntoView()
+		},
+		loadFavs () {
+			const data = localStorage.getItem('favs')
+			if (data) {
+				try {
+					return JSON.parse(data)
+				} catch {
+					localStorage.setItem('favs', '[]')
+				}
+			}
+			return []
+		},
+		pruneFavs (favs, sessions) {
+			const talks = sessions || []
+			const talkIds = talks.map(e => e.id)
+			return favs.filter(e => talkIds.includes(e))
+		},
+		saveFavs () {
+			localStorage.setItem('favs', JSON.stringify(this.favs))
+		},
+		fav (id) {
+			if (!this.favs.includes(id)) {
+				this.favs.push(id)
+				this.saveFavs()
+			}
+		},
+		unfav (id) {
+			this.favs = this.favs.filter(elem => elem !== id)
+			this.saveFavs()
+			if (!this.favs.length) this.onlyFavs = false
 		}
 	}
 }
@@ -87,4 +154,6 @@ export default {
 		h1
 			font-size: 3vw
 			text-align: center
+	.c-grid-schedule .grid > .room
+		top: 0
 </style>
