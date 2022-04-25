35e1184ce0211c076aa096ffa5b824790608abb8
Raphael Michel <michel@rami.io>
2022-04-25 13:53:33 +0200

    [server] Update to channels_redis 3.4

--- server/venueless/platforms/channels/pubsub.py
+++ server/venueless/platforms/channels/pubsub.py
@@ -4,11 +4,13 @@ from channels_redis.pubsub import RedisPubSubChannelLayer
 class ChannelLayer(RedisPubSubChannelLayer):
     async def group_discard(self, group, channel):
         # Silently fail if group currently not subscribed
-        layer = self._get_layer()
-        group_channel = layer._get_group_channel_name(group)
-        if group_channel not in layer.groups:
+        group_channel = self._get_group_channel_name(group)
+        if group_channel not in self.groups:
             return
-        group_channels = layer.groups[group_channel]
-        if channel not in group_channels:
-            return
-        return await super()._proxy("group_discard", group, channel)
+        group_channels = self.groups[group_channel]
+        assert channel in group_channels
+        group_channels.remove(channel)
+        if len(group_channels) == 0:
+            del self.groups[group_channel]
+            shard = self._get_shard(group_channel)
+            await shard.unsubscribe(group_channel)
