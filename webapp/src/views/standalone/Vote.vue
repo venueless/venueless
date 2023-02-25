5f45affe83db601dca036febef798f6cf07f40ab
rash <rash@rash.codes>
2023-02-25 18:26:33 +0100

    add vote qrcode

--- webapp/src/views/standalone/Vote.vue
+++ webapp/src/views/standalone/Vote.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
-.c-kiosk-slide-vote
-	h1 VOTE!!
+.c-standalone-vote
+	h1 Vote now here:
+	.svg(v-html="qrcode")
+	.url {{ url }}
 </template>
 <script>
+import QRCode from 'qrcode'
+
 export default {
 	components: {},
 	props: {
@@ -10,10 +14,29 @@ export default {
 	},
 	data () {
 		return {
+			qrcode: null
 		}
 	},
-	computed: {},
+	computed: {
+		url () {
+			return `vle.ss/AB12`
+		},
+		fullUrl () {
+			return `https://${this.url}`
+		}
+	},
+	async mounted () {
+		this.qrcode = await QRCode.toString(this.url, {type: 'svg'})
+	}
 }
 </script>
 <style lang="stylus">
+.c-standalone-vote
+	display: flex
+	flex-direction: column
+	align-items: center
+	.svg
+		width: 500px
+	.url
+		font-size: 48px
 </style>
