bc551c0e3a320ebd889666403f2b4b4ee5cf73c4
rash <rash@rash.codes>
2023-08-20 21:14:21 +0200

    [anonymous] add frontend auth with invite short code

---
+++ webapp/src/views/standalone/Anonymous.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+</template>
+<script>
+export default {
+	components: {},
+	data () {
+		return {
+		}
+	},
+	computed: {},
+	async created () {},
+	async mounted () {
+		await this.$nextTick()
+	},
+	methods: {}
+}
+</script>
+<style lang="stylus">
+</style>
