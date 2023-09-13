50d255772cb17ed59b6cd2aea4875a5f75b8c8e1
Raphael Michel <michel@rami.io>
2023-09-13 16:44:48 +0200

    Re-enable async runserver

--- server/venueless/settings.py
+++ server/venueless/settings.py
@@ -246,6 +246,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "channels",
+    "daphne",
     "corsheaders",
     "rest_framework",
     "djangoformsetjs",
