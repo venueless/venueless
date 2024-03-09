178516cd629ca31e9d1a7da2a662871b341052b5
Raphael Michel <michel@rami.io>
2024-03-09 15:27:33 +0100

    Web push support

---
+++ server/venueless/live/tasks.py
@@ -0,0 +1,30 @@
+import json
+import logging
+
+from django.conf import settings
+from pywebpush import WebPushException, webpush
+
+from venueless.celery_app import app
+from venueless.core.models import World
+from venueless.core.tasks import WorldTask
+
+logger = logging.getLogger(__name__)
+
+
+@app.task(base=WorldTask)
+def send_web_push(world: World, user_id: int, data: dict):
+    user = world.users.get(user_id)
+    for client in user.web_push_clients.all():
+        try:
+            webpush(
+                subscription_info=client.subscription,
+                data=json.dumps(data),
+                vapid_private_key=world.vapid_private_key,
+                vapid_claims={
+                    "sub": f"mailto:{settings.SERVER_EMAIL}",
+                },
+            )
+        except WebPushException as ex:
+            logging.warning(
+                f"Could not send web push notification to client {client.pk} of user {user.pk}. Exception: {ex}"
+            )
