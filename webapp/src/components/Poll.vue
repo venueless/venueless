b5dd94c7a09e8b8dc4b3f44a0655c942352db3fc
Raphael Michel <michel@rami.io>
2025-02-10 12:59:17 +0100

    Update webapp/src/components/Poll.vue
    Co-authored-by: rash <rash@rash.codes>

--- webapp/src/components/Poll.vue
+++ webapp/src/components/Poll.vue
@@ -5,7 +5,8 @@
 		bunt-button.btn-option(v-for="option of poll.options", @click="$store.dispatch('poll/vote', {poll, option})") {{ option.content }}
 	template(v-else)
 		.option(v-for="option of poll.options", :class="{'most-votes': optionsWithMostVotes.includes(option.id)}")
-			.content {{ option.content }}
+			.content
+				| {{ option.content }}
 				span.mdi.mdi-check-circle-outline(v-if="poll.answers && poll.answers.includes(option.id)", v-tooltip="$t('Poll:your-choice')")
 			.votes(:style="{'--votes': poll.results[option.id]}") {{ totalVotes ? (poll.results[option.id] / totalVotes * 100).toFixed() : 0 }}%
 	.actions(v-if="isManaging && hasPermission('room:poll.manage')")
