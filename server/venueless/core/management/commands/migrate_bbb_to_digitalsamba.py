5d17bb5ec2635bc896aedd92bc248ec3188364e1
Raphael Michel <michel@rami.io>
2025-02-24 12:12:31 +0100

    Fix migration command

--- server/venueless/core/management/commands/migrate_bbb_to_digitalsamba.py
+++ server/venueless/core/management/commands/migrate_bbb_to_digitalsamba.py
@@ -3,6 +3,7 @@ import logging
 
 from asgiref.sync import sync_to_async
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from venueless.core.models import World
 from venueless.core.permissions import Permission
@@ -14,58 +15,58 @@ class Command(BaseCommand):
     help = "Migrate worlds from BBB to DigitalSamba"
 
     def handle(self, *args, **options):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.do())
+        asyncio.run(self.do())
 
     async def do(self):
         # Yeah this is necessary for event loop creation
         await sync_to_async(self._do)()
 
     def _do(self):
-        for w in World.objects.all():
-            new_roles = {}
-            for k, v in w.roles.items():
-                if k == "viewer" and Permission.ROOM_DIGITALSAMBA_JOIN not in v:
-                    v.append(Permission.ROOM_DIGITALSAMBA_JOIN)
-                if (
-                    Permission.ROOM_BBB_JOIN in v
-                    and Permission.ROOM_DIGITALSAMBA_JOIN not in v
-                ):
-                    v.append(Permission.ROOM_DIGITALSAMBA_JOIN)
-                    if k != "viewer":
-                        v.append(Permission.ROOM_DIGITALSAMBA_SPEAK)
-                if (
-                    Permission.ROOM_BBB_MODERATE in v
-                    and Permission.ROOM_DIGITALSAMBA_MODERATE not in v
-                ):
-                    v.append(Permission.ROOM_DIGITALSAMBA_MODERATE)
-                if (
-                    Permission.ROOM_BBB_RECORDINGS in v
-                    and Permission.ROOM_DIGITALSAMBA_RECORDINGS not in v
-                ):
-                    v.append(Permission.ROOM_DIGITALSAMBA_RECORDINGS)
-                new_roles[k] = v
-            w.roles = new_roles
-            if "digitalsamba" not in w.feature_flags:
-                w.feature_flags = [*w.feature_flags, "digitalsamba"]
-            w.save()
+        with transaction.atomic():
+            for w in World.objects.all():
+                new_roles = {}
+                for k, v in w.roles.items():
+                    if k == "viewer" and Permission.ROOM_DIGITALSAMBA_JOIN.value not in v:
+                        v.append(Permission.ROOM_DIGITALSAMBA_JOIN.value)
+                    if (
+                        Permission.ROOM_BBB_JOIN.value in v
+                        and Permission.ROOM_DIGITALSAMBA_JOIN.value not in v
+                    ):
+                        v.append(Permission.ROOM_DIGITALSAMBA_JOIN.value)
+                        if k != "viewer":
+                            v.append(Permission.ROOM_DIGITALSAMBA_SPEAK.value)
+                    if (
+                        Permission.ROOM_BBB_MODERATE.value in v
+                        and Permission.ROOM_DIGITALSAMBA_MODERATE.value not in v
+                    ):
+                        v.append(Permission.ROOM_DIGITALSAMBA_MODERATE.value)
+                    if (
+                        Permission.ROOM_BBB_RECORDINGS.value in v
+                        and Permission.ROOM_DIGITALSAMBA_RECORDINGS.value not in v
+                    ):
+                        v.append(Permission.ROOM_DIGITALSAMBA_RECORDINGS.value)
+                    new_roles[k] = v
+                w.roles = new_roles
+                if "digitalsamba" not in w.feature_flags:
+                    w.feature_flags = [*w.feature_flags, "digitalsamba"]
+                w.save()
 
-            for r in w.rooms.all():
-                for m in r.module_config:
-                    if m["type"] == "call.bigbluebutton":
-                        m["type"] = "call.digitalsamba"
-                        m["_old_bbb_config"] = m["config"]
-                        m["config"] = {
-                            "size": "small",
-                            "tiles": "cam_mic",
-                            "waiting_room": m.get("config", {}).get(
-                                "waiting_room", False
-                            ),
-                            "mute_on_start": not m.get("config", {}).get(
-                                "auto_microphone", False
-                            ),
-                            "disable_cam_on_start": not m.get("config", {}).get(
-                                "auto_camera", False
-                            ),
-                        }
-                r.save()
+                for r in w.rooms.all():
+                    for m in r.module_config:
+                        if m["type"] == "call.bigbluebutton":
+                            m["type"] = "call.digitalsamba"
+                            m["_old_bbb_config"] = m["config"]
+                            m["config"] = {
+                                "size": "small",
+                                "tiles": "cam_mic",
+                                "waiting_room": m.get("config", {}).get(
+                                    "waiting_room", False
+                                ),
+                                "mute_on_start": not m.get("config", {}).get(
+                                    "auto_microphone", False
+                                ),
+                                "disable_cam_on_start": not m.get("config", {}).get(
+                                    "auto_camera", False
+                                ),
+                            }
+                    r.save()
