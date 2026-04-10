802ddf440e16993f93400592f3c90915e2e0d8a9
Raphael Michel <michel@rami.io>
2026-04-10 11:44:57 +0200

    [SECURITY] Prevent SSRF in link preview

--- server/venueless/core/__init__.py
+++ server/venueless/core/__init__.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = "venueless.core"
     label = "core"
+
+    def ready(self):
+        from .monkeypatching import monkeypatch_all_at_ready
+        monkeypatch_all_at_ready()
