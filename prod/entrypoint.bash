4f01d73d7e2b3391b9cad0231403b30a3fab2657
Raphael Michel <michel@rami.io>
2026-06-22 11:09:16 +0200

    Dockerfile: Explicit binary paths

--- prod/entrypoint.bash
+++ prod/entrypoint.bash
@@ -4,6 +4,7 @@ export DJANGO_SETTINGS_MODULE=venueless.settings
 export VENUELESS_DATA_DIR=/data/
 export HOME=/venueless
 export NUM_WORKERS=${VENUELESS_WORKERS:-$((2 * $(nproc --all)))}
+export BINDIR=/opt/venv/bin
 
 if [ ! -d /data/logs ]; then
     mkdir /data/logs;
@@ -13,21 +14,21 @@ if [ ! -d /data/media ]; then
 fi
 
 if [ "$1" == "all" ]; then
-    python3 manage.py migrate --noinput
+    $BINDIR/python manage.py migrate --noinput
     exec sudo -E /usr/bin/supervisord -n -c /etc/supervisord.conf
 fi
 
 if [ "$1" == "celery" ]; then
-    exec celery -A venueless.celery_app worker -l info
+    exec $BINDIR/celery -A venueless.celery_app worker -l info
 fi
 
 if [ "$1" == "webworker" ]; then
     mkdir -p /tmp/venueless
-    exec gunicorn -k uvicorn.workers.UvicornWorker --bind unix:/tmp/venueless/websocket.sock -w "$NUM_WORKERS" venueless.asgi:application
+    exec $BINDIR/gunicorn -k uvicorn.workers.UvicornWorker --bind unix:/tmp/venueless/websocket.sock -w "$NUM_WORKERS" venueless.asgi:application
 fi
 
 if [ "$1" == "shell" ]; then
-    exec python3 manage.py shell_plus
+    exec $BINDIR/python manage.py shell_plus
 fi
 
-exec python3 manage.py "$@"
+exec $BINDIR/python manage.py "$@"
